f94873742f1278722f3f752e1bc7ab12784426f6
Alexandr Kolesnik <alexandr.kolesnik.92@gmail.com>
2017-04-03 19:18:30 -0700

    Basic functionality for GUI. Supports multiple splits.

--- gui.py
+++ gui.py
@@ -2,45 +2,86 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 
-class Window(QMainWindow):
-    splits = []
+from ffmpeg import FFMpeg
+import utils
+
+class Window(QWidget):
+    segmentLayout = QVBoxLayout()
+    segments = []
 
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.statusBar().showMessage('Ready')
-        self.resize(320, 240)
+        timestamps = QLabel('Timestamps')
+        name = QLabel('Name')
+
+        self.inputEdit = QLineEdit()
+
+        inputBtn = QPushButton('Video')
+        inputBtn.setShortcut('Ctrl+O')
+        inputBtn.setStatusTip('Select video to split.')
+        inputBtn.clicked.connect(self.inputFileDialog)
+
+        runBtn = QPushButton('Run')
+        runBtn.clicked.connect(self.run)
+
+        addBtn = QPushButton('Add')
+        addBtn.clicked.connect(self.addSegment)
+
+        labelLayout = QHBoxLayout()
+        labelLayout.addWidget(timestamps)
+        labelLayout.addWidget(name)
+
+        runLayout = QHBoxLayout()
+        runLayout.addWidget(inputBtn)
+        runLayout.addWidget(self.inputEdit)
+        runLayout.addWidget(addBtn)
+        runLayout.addWidget(runBtn)
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addLayout(labelLayout)
+        mainLayout.addLayout(self.segmentLayout)
+        mainLayout.addLayout(runLayout)
+
+        self.setLayout(mainLayout)
+
+        self.addSegment()
+
         self.setWindowTitle('Video Splitter')
-
-        self.input_le = QLineEdit(self)
-        self.input_le.move(130, 22)
-
-        # Button to select video
-        self.input_btn = QPushButton('Video', self)
-        self.input_btn.move(20, 20)
-        self.input_btn.setShortcut('Ctrl+O')
-        self.input_btn.setStatusTip('Select video to split.')
-        self.input_btn.clicked.connect(self.inputFileDialog)
-
         self.show()
+
+    def addSegment(self):
+        hlayout = QHBoxLayout()
+
+        timestampsEdit = QLineEdit()
+        nameEdit = QLineEdit()
+
+        hlayout.addWidget(timestampsEdit)
+        hlayout.addWidget(nameEdit)
+
+        self.segmentLayout.addLayout(hlayout)
+
+        self.segments.append({
+            'timestamps': timestampsEdit,
+            'name': nameEdit,
+        })
+
 
     def inputFileDialog(self):
         self.input = QFileDialog.getOpenFileName(self, 'Select video to split', '.')
         if self.input:
-            print(self.input[0])
-            self.input_le.setText(str(self.input[0]))
+            self.inputEdit.setText(str(self.input[0]))
 
-    def addSplit(self):
-        time_le = QLineEdit(self)
-        if len(self.splits) > 0:
-            y = self.splits[-1]['y'] + 33
-        else:
-            y = 0
-        time_le.move(0, y)
-        self.splits.append({ 'le': time_le, 'x': 0, 'y': y })
-
+    def run(self):
+        input = self.inputEdit.displayText()
+        for segment in self.segments:
+            name  = segment['name'].displayText()
+            timestamps = utils.timestr_to_timestamps(segment['timestamps'].displayText())
+            start = timestamps['start']
+            end = timestamps['end']
+            FFMpeg(input, name, start, end).run()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
